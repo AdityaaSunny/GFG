5a028786fc18828a3c51ea41d9efdef029017c3a
Aditya Kumar <95024430+AdityaaSunny@users.noreply.github.com>
2024-05-19 01:31:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the closest number/find-the-closest-number.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int findClosest( int n, int k,int arr[]) 
+    { 
+        // Complete the function
+        int min_diff=INT_MAX;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            
+            int diff=abs(arr[i]-k);
+            if(diff<=min_diff){
+                min_diff=diff;
+                ans=arr[i];
+            }
+        }
+        return ans;
+    } 
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.findClosest(n,k,arr)<<endl;
+    }
+}
+
+// } Driver Code Ends
